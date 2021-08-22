c6591e16922b073c7e5c5e18f9affb0dd3d51552
Evgeny <evgenyostaltsev@gmail.com>
2021-08-22 22:06:38 +0300

    add me

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,26 @@
+$(document).ready(function(){
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST", 
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            // $('#consultation, #order').fadeOut();
+            // $('.overlay, #thanks').fadeIn('slow');
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+
+});
+
+
 const hamburger = document.querySelector('.hamburger'),
       menu = document.querySelector('.menu'),
+      menuLink = document.querySelectorAll('.menu__link'),
       closeElem = document.querySelector('.menu__close');
 
 hamburger.addEventListener('click', () => {
@@ -10,9 +31,16 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+menuLink.forEach(item => {
+    item.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
+
 const counters = document.querySelectorAll('.skills__ratings-counter'),
       lines = document.querySelectorAll('.skills__ratings-line span');
 
 counters.forEach((item, i) => {
     lines[i].style.width = item.innerHTML;
 });
+
